db038dffd16179bb95a3df0d57a7c132bfd9b19d
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-09-04 13:36:31 -0300

    Create CityEvent and EventReservation

---
+++ APIEvent.Core/Model/CityEvent.cs
@@ -0,0 +1,29 @@
+﻿namespace APIEvent.Core.Model
+{
+    public class CityEvent
+    {
+        public CityEvent(string title, string local, DateTime dateHourEvent, string? description = null, string? address = null, decimal? preco = null)
+        {
+            Title = title;
+            Local = local;
+            DateHourEvent = dateHourEvent;
+            Description = description;           
+            Address = address;
+            Preco = preco;
+        }
+
+        public long IdEvent {get; set;}
+
+        public string Title {get; set;} 
+        
+        public string? Description { get; set; }
+
+        public DateTime DateHourEvent { get; set; }
+        
+        public string Local { get; set; }      
+
+        public string? Address { get; set; }
+
+        public decimal? Preco { get; set; } 
+    }
+}
---
+++ APIEvent.Core/Model/EventReservation.cs
@@ -0,0 +1,18 @@
+﻿namespace APIEvent.Core.Model
+{
+    public class EventReservation
+    {
+        public EventReservation(long idEvent, string personName, long quantity)
+        {
+            IdEvent = idEvent;
+            PersonName = personName;
+            Quantity = quantity;
+        }
+
+     public long IdEvent { get; set; }
+     public long IdReservation { get; set; }
+     public string PersonName { get; set; }
+     public long Quantity { get; set; }
+
+    }
+}
