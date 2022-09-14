c7bf6e423b7c363636c25053c57a56d3bbc72108
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-09-13 22:56:58 -0300

    feat application of DataAnnotations in CityEvent and EventReservation

--- APIEvent.Core/Model/EventReservation.cs
+++ APIEvent.Core/Model/EventReservation.cs
@@ -1,13 +1,22 @@
-﻿namespace APIEvent.Core.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIEvent.Core.Model
 {
     public class EventReservation
     {
 
+        [Required(ErrorMessage = "Id do evento é obrigatório")]
+        public long IdEvent { get; set; }
 
-     public long IdEvent { get; set; }
-     public long IdReservation { get; set; }
-     public string PersonName { get; set; }
-     public long Quantity { get; set; }
+        public long IdReservation { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [MaxLength(100, ErrorMessage = "O nome pode ter até 100 caracteres")]
+        [MinLength(3, ErrorMessage = "O nome deve ter mais que 2 caracteres")]
+        public string PersonName { get; set; }
+        
+        [Required(ErrorMessage = "Quantidade é obrigatória")]
+        public long Quantity { get; set; }
 
     }
 }
