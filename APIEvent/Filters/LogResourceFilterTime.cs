61138ed9284184eb24bee77beb3c9a3d25a09468
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-09-16 16:50:21 -0300

    feat LogResourceFilterTime

---
+++ APIEvent/Filters/LogResourceFilterTime.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace APIEvent.Filters
+{
+
+    public class LogResourceFilterTime : IResourceFilter
+    {
+        Stopwatch time = new Stopwatch();
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            time.Stop();
+            Console.WriteLine($"Tempo do processo: {time.ElapsedMilliseconds} milissegundos");
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            time.Start();
+        }
+    }
+}
+
+
